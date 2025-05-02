f219747ea35afb5b00325ea9ff709e50aa563eb1
dani_villalobos <38198425+DaniVillalobos@users.noreply.github.com>
2025-05-02 18:19:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ logging.basicConfig(filename='api_logs.log', level=logging.DEBUG,
                     format='%(asctime)s:%(name)s:%(module)s:%(levelname)s:%(message)s')
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    logging.info("Hello World!")
+    return {"Hello": "World"}
+    
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    logging.debug(f"item_id: {item_id}")
+    return {"item_id": item_id, "q": q}
